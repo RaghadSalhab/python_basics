7e7a162a01381126d0dcbdba35637511bbe90f08
RaghadSalhab <raghadsalhab13@gmail.com>
2025-09-21 14:41:36 +0200

    updating guess game

--- GuessClassWay.py
+++ GuessClassWay.py
@@ -10,8 +10,8 @@ class GuessGame:
         return cls.instance
 
     def __init__(self,min,max):
-        self.target=self.generate_random(min,max)
-        self.trail_count=0
+        self.min=min
+        self.max=max
         self.attempts=[]
 
     def generate_random(self,min,max):
@@ -20,7 +20,7 @@ class GuessGame:
     def guess_num(self,num):
         self.attempts.append(num)
         self.trail_count+=1
-        
+
         if(self.target==num):
             print(f'YOU GUESS IT GOOD! its {self.target} and the number of trail is {self.trail_count} ')
             return True
@@ -32,6 +32,11 @@ class GuessGame:
         return False
 
     def play(self):
+        self.target=self.generate_random(self.min,self.max)
+        self.trail_count=0
+        print(f'the guessing num is : { self.target}')
+        
+        print('start a new game')
         while True:
             try:
                 num = int(input('enter number: '))
@@ -47,8 +52,10 @@ class GuessGame:
         return iter(self.attempts)
 
 p=GuessGame(1,100)
-print(p)
-p.play()
-
-for attempt in p:
-    print("Tried:", attempt)
+while True:
+    choice=input('do you what to play (yes, no): ')
+    if choice.lower()=='yes':
+        p.play()
+    else:
+        print('finish the program')
+        break
